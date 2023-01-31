0ca35778b7eebc138232b09766c687165f726db2
ali <iamaliahad@outlook.com>
2023-01-31 20:03:05 +0800

    ok

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
         const fetchPost = async () => {
             setLoading(true);
             try {
-                const response = await fetch("http://localhost:8080/api/v1/post", {
+                const response = await fetch("https://share-your-throught-and-generate-to-image-server.vercel.app/api/v1/post", {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
