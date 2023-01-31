8bbe31f94899632875ef69969e123bd6273dcc55
ali <iamaliahad@outlook.com>
2023-02-01 00:22:48 +0800

    changes some color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,17 @@ import { Home, CreatePost } from './pages/index'
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-y py-4 border-b border-b-[e6ebf4]'>
+      <header className='w-full flex justify-between items-center bg-white dark:bg-[#0D0D0D] sm:px-8 px-y py-4 border-b border-gray-100'>
         <Link to="/">
           <div className='flex display-inline'>
-            <h1 className='w-28 object-contain mb-1'>ShowYourThrought</h1>
+            <h1 className='w-28 text-white mb-1'>ShowYourThrought</h1>
           </div>
         </Link>
         <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
           Create image
         </Link>
       </header>
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#0D0D0D] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
